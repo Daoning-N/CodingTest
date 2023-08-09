3c8c731ed8db54d20ffb611ac11639d4b1542693
Daon <121539291+Daoning-N@users.noreply.github.com>
2023-08-10 03:14:26 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        for(int i=0; i<age; i++){
+            answer = 2022-i;
+        }
+        return answer;
+    }
+}
