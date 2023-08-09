7f3443f2dab85e0d3dd6f91e24a8e519527f9bdb
Daon <121539291+Daoning-N@users.noreply.github.com>
2023-08-10 00:37:08 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        if(price>=100000 && 300000>price){
+            answer = (int)(price-(price*0.05));
+        }else if(price>=300000 && 500000>price){
+            answer = (int)(price-(price*0.1));
+        }else if(price>=500000){
+            answer = (int)(price-(price*0.2));
+        }else{
+            answer = price;
+        }
+        return answer;
+    }
+}
