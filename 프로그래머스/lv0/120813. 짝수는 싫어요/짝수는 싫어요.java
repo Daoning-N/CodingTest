2479d79efabae6ae1f9970e5f7429faf0a872428
Daon <121539291+Daoning-N@users.noreply.github.com>
2023-08-14 22:36:43 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,15 +1,12 @@
 import java.util.*;
-
 class Solution {
     public ArrayList solution(int n) {
         ArrayList<Integer> answer = new ArrayList<>();
-
         for(int i=1; i<=n; i++){
-          if(i%2 != 0) {
-              answer.add(i);
-          } 
+            if(i%2!=0){
+                answer.add(i);
+            }
         }
-
         return answer;
     }
 }
