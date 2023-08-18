009b9ba67af66c82d82e3bee09a07653ef81ccc9
Daon <121539291+Daoning-N@users.noreply.github.com>
2023-08-18 20:23:24 +0900

    [Silver IV] Title: 주몽, Time: 216 ms, Memory: 16452 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        int count = 0;
+        int i = 0; //A[0] -> min
+        int j = N-1; //A[N-1] -> max
+        while(i<j){
+            if(A[i]+A[j]<M){
+                i++;
+            }else if(A[i]+A[j]>M){
+                j--;
+            }else{
+                count++; //갑옷
+                i++; 
+                j--;
+            }
+        }
+        System.out.println(count);
+    }
+}
