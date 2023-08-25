0cc4ba643c8a4483317df78835a555d8bfaf0c3b
Daon <121539291+Daoning-N@users.noreply.github.com>
2023-08-25 18:56:56 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.22 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String []numbers = my_string.replaceAll("[^0-9]","").split("");
+        for(String n : numbers){
+            answer += Integer.parseInt(n);
+        }
+        return answer;
+    }
+}
