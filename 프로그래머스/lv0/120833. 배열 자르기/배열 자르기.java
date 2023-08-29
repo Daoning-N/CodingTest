17f1d2356d670325baae400d9f84fa0ac5e69cc8
Daon <121539291+Daoning-N@users.noreply.github.com>
2023-08-29 21:33:45 +0900

    [level 0] Title: 배열 자르기, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -1,10 +1,13 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int[num2-num1+1];
-        int cnt = 0;
-        for(int i = num1; i <= num2; i++){
-            answer[cnt] = numbers[i];
-            cnt++;
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=num1; i<=num2; i++){
+            arr.add(numbers[i]);
+        }
+        int [] answer = new int[arr.size()];
+        for(int i=0; i<arr.size(); i++){
+            answer[i] = arr.get(i);
         }
         return answer;
     }
